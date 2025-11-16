e6253148cc38443d250acd7bdaaecc0bff1d885a
HuaMouRen <2825338723@qq.com>
2025-11-16 21:02:18 +0800

    ref(controls): 更改UserCard显示逻辑

---
+++ PvzLauncherRemake/Controls/UserCard.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace PvzLauncherRemake.Controls
+{
+    public class UserCard:ContentControl
+    {
+        static UserCard()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(UserCard), new FrameworkPropertyMetadata(typeof(UserCard)));
+        }
+    }
+}
