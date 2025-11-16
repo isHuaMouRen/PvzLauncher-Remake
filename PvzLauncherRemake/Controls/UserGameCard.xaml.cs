88db2e5dcf24fb2483fd63a34b103a3825bc7198
HuaMouRen <2825338723@qq.com>
2025-11-16 19:55:54 +0800

    feat(controls): 新增UserGameCard控件

---
+++ PvzLauncherRemake/Controls/UserGameCard.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PvzLauncherRemake.Controls
+{
+    /// <summary>
+    /// UserGameCard.xaml 的交互逻辑
+    /// </summary>
+    public partial class UserGameCard : UserControl
+    {
+        public Path[] Icon { get; set; } = null!;
+        public string Title { get; set; } = "Title";
+        public string Version { get; set; } = "1.0.0.0";
+
+
+        public UserGameCard()
+        {
+            InitializeComponent();
+
+            Loaded += LoadUI;
+        }
+
+        private void LoadUI(object sender, RoutedEventArgs e)
+        {
+            grid_Icon.Children.Clear();
+            foreach (var path in Icon)
+            {
+                grid_Icon.Children.Add(path);
+            }
+
+            textBlock_Title.Text = Title;
+            textBlock_Version.Text = Version;
+        }
+    }
+}
