23bce631c1c8106d4618ade64ed81d2b9082b76a
HuaMouRen <2825338723@qq.com>
2025-11-16 13:10:07 +0800

    feat(app-logger): 添加全局类AppLogger

---
+++ PvzLauncherRemake/Class/AppLogger.cs
@@ -0,0 +1,11 @@
+using System.IO;
+using HuaZi.Library.Logger;
+
+namespace PvzLauncherRemake.Class
+{
+    public static class AppLogger
+    {
+        //日志记录器
+        public static Logger logger = new Logger(Path.Combine(AppInfo.ExecutePath, "Logs"));
+    }
+}
