57cbfe5b2f729d8f3dbaec9442d2638605cd2fc4
HuaMouRen <2825338723@qq.com>
2025-11-16 13:09:52 +0800

    feat(app-info): 添加全局类AppInfo

---
+++ PvzLauncherRemake/Class/AppInfo.cs
@@ -0,0 +1,12 @@
+using System.IO;
+using System.Reflection;
+
+namespace PvzLauncherRemake.Class
+{
+    //全局类
+    public static class AppInfo
+    {
+        public static readonly string ExecutePath = $"{Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)}";//执行目录
+        public static readonly string Version = $"1.0.0-alpha.1";//版本
+    }
+}
