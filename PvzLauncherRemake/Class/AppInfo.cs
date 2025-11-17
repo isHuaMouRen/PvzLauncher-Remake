e861df422cc493a515552e1ba8a1c7dc2468496a
HuaMouRen <2825338723@qq.com>
2025-11-17 11:03:10 +0800

    feat(app-info): 添加启动参数全局变量

--- PvzLauncherRemake/Class/AppInfo.cs
+++ PvzLauncherRemake/Class/AppInfo.cs
@@ -8,5 +8,6 @@ namespace PvzLauncherRemake.Class
     {
         public static readonly string ExecutePath = $"{Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)}";//执行目录
         public static readonly string Version = $"1.0.0-alpha.2";//版本
+        public static readonly string[] Arguments = Environment.GetCommandLineArgs();//启动参数
     }
 }
