b136170fde42148071a58b05a6e0285896d28fbc
HuaMouRen <2825338723@qq.com>
2025-11-18 20:17:51 +0800

    feat(download): 更新下载功能

--- PvzLauncherRemake/Pages/PageDownload.xaml.cs
+++ PvzLauncherRemake/Pages/PageDownload.xaml.cs
@@ -1,11 +1,15 @@
 ﻿using HuaZi.Library.Downloader;
 using HuaZi.Library.Json;
+using ModernWpf.Controls;
+using Notifications.Wpf;
 using PvzLauncherRemake.Class;
 using PvzLauncherRemake.Class.JsonConfigs;
 using PvzLauncherRemake.Controls;
 using PvzLauncherRemake.Utils;
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
@@ -29,21 +33,25 @@ namespace PvzLauncherRemake.Pages
     /// </summary>
     public partial class PageDownload : ModernWpf.Controls.Page
     {
+        private NotificationManager notificationManager = new NotificationManager();
         private JsonDownloadIndex.Index DownloadIndex = null!;
         private Downloader downloader = null!;
 
         #region Load
-        public void StartLoad()
+        public void StartLoad(bool showProgressBar = false)
         {
             tabControl_Main.IsEnabled = false;
             tabControl_Main.Effect = new BlurEffect { Radius = 10 };
             grid_Loading.Visibility = Visibility.Visible;
+            if (showProgressBar)
+                progressBar_Loading.Visibility = Visibility.Visible;
         }
         public void EndLoad()
         {
             tabControl_Main.IsEnabled = true;
             tabControl_Main.Effect = null;
             grid_Loading.Visibility = Visibility.Hidden;
+            progressBar_Loading.Visibility = Visibility.Hidden;
         }
         #endregion
 
@@ -127,5 +135,151 @@ namespace PvzLauncherRemake.Pages
         }
 
         private void Page_Loaded(object sender, RoutedEventArgs e) { InitializeLoaded(); }
+
+        private async void listBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            try
+            {
+                logger.Info("开始下载游戏...");
+
+                var listbox = (ListBox)sender;
+                var selectItem = (UserDownloadCard)listbox.SelectedItem;
+                var downloadIndex = (JsonDownloadIndex.GameInfo)selectItem.Tag;
+                string savePath = System.IO.Path.Combine(AppInfo.GameDirectory, downloadIndex.Name);
+
+                await DialogManager.ShowDialogAsync(new ContentDialog
+                {
+                    Title = "下载确认",
+                    Content = $"名称: {downloadIndex.Name}\n\n版本: {downloadIndex.Version}",
+                    PrimaryButtonText = "开始下载",
+                    CloseButtonText = "取消",
+                    DefaultButton = ModernWpf.Controls.ContentDialogButton.Primary
+                }, (async () =>
+                {
+                    try
+                    {
+                        //检测同名
+                        bool isNameDone = false;
+                        while (!isNameDone)
+                        {
+                            var textBox = new TextBox { Text = downloadIndex.Name };
+
+                            if (Directory.Exists(System.IO.Path.Combine(AppInfo.GameDirectory, downloadIndex.Name)))
+                            {
+                                await DialogManager.ShowDialogAsync(new ContentDialog
+                                {
+                                    Title = "发现冲突",
+                                    Content = new StackPanel
+                                    {
+                                        Children =
+                                        {
+                                            new TextBlock
+                                            {
+                                                Text=$"在您的游戏库发现与 \"{downloadIndex.Name}\" 同名的文件夹，请使用其他名称",
+                                                Margin=new Thickness(0,0,0,10)
+                                            },
+                                            textBox
+                                        }
+                                    },
+                                    PrimaryButtonText = "确定",
+                                    DefaultButton = ContentDialogButton.Primary
+                                });
+                                if (textBox.Text != downloadIndex.Name)
+                                {
+                                    savePath = System.IO.Path.Combine(AppInfo.GameDirectory, textBox.Text);
+                                    isNameDone = true;
+                                }
+                            }
+                            else
+                                isNameDone = true;
+                        }
+                        StartLoad(true);
+
+                        //清除残留
+                        if (File.Exists(System.IO.Path.Combine(AppInfo.TempPath, "Game.zip")))
+                            await Task.Run(() => File.Delete(System.IO.Path.Combine(AppInfo.TempPath, "Game.zip")));
+
+                        bool isDownloadDone = false;
+                        string? downloadError = null;
+                        //定义下载器
+                        downloader = new Downloader
+                        {
+                            Url = downloadIndex.Url,
+                            SavePath = System.IO.Path.Combine(AppInfo.TempPath, "Game.zip"),
+                            Progress = ((p, s) =>
+                            {
+                                progressBar_Loading.Value = p;
+                                textBlock_Loading.Text = $"下载中 {Math.Round(p, 2)}% ... ({Math.Round(s / 1024, 2)}MB/S)";
+                            }),
+                            Completed = ((s, e) =>
+                            {
+                                if (s)
+                                    isDownloadDone = true;
+                                else
+                                {
+                                    downloadError = e;
+                                    isDownloadDone = true;
+                                }
+                            })
+                        };
+
+                        downloader.StartDownload();
+
+                        //等下载完毕
+                        while (isDownloadDone == false)
+                            await Task.Delay(1000);
+
+                        //下载失败抛错误
+                        if (!string.IsNullOrEmpty(downloadError))
+                            throw new Exception(downloadError);
+
+
+                        textBlock_Loading.Text = "解压中...";
+                        //解压
+                        await Task.Run(() => ZipFile.ExtractToDirectory(System.IO.Path.Combine(AppInfo.TempPath, "Game.zip"), savePath, true));
+
+                        //写Json
+                        var jsonContent = new JsonGameInfo.Index
+                        {
+                            GameInfo = new JsonGameInfo.GameInfo
+                            {
+                                ExecuteName = downloadIndex.ExecuteName,
+                                Name = System.IO.Path.GetFileName(savePath),
+                                Version = downloadIndex.Version,
+                                VersionType = downloadIndex.VersionType
+                            },
+                            Record = new JsonGameInfo.Record
+                            {
+                                FirstPlay = DateTimeOffset.Now.ToUnixTimeSeconds(),
+                                PlayCount = 0,
+                                PlayTime = 0
+                            }
+                        };
+                        Json.WriteJson(System.IO.Path.Combine(savePath, ".pvzl.json"), jsonContent);
+
+                        notificationManager.Show(new NotificationContent
+                        {
+                            Title = "下载完成",
+                            Message = $"\"{jsonContent.GameInfo.Name}\" 已添加进您的游戏库!",
+                            Type = NotificationType.Success
+                        });
+
+                        //设置当前项
+                        AppInfo.Config.CurrentGame = jsonContent.GameInfo.Name;
+                        NavigationController.Navigate(this, "Manage");
+
+                        EndLoad();
+                    }
+                    catch (Exception ex)
+                    {
+                        ErrorReportDialog.Show("发生错误", "下载发生错误", ex);
+                    }
+                }));
+            }
+            catch (Exception ex)
+            {
+                ErrorReportDialog.Show("发生错误", "下载游戏时发生错误", ex);
+            }
+        }
     }
 }
