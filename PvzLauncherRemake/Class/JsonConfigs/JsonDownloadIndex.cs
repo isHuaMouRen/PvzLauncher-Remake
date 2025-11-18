bb2a85069f6adccd734afccebb13ebd9cea1da14
HuaMouRen <2825338723@qq.com>
2025-11-18 16:41:01 +0800

    feat(json-configs): 添加配置JsonDownloadIndex

---
+++ PvzLauncherRemake/Class/JsonConfigs/JsonDownloadIndex.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PvzLauncherRemake.Class.JsonConfigs
+{
+    public class JsonDownloadIndex
+    {
+        public partial class Download
+        {
+            [JsonProperty("en_origin")]
+            public GameInfo[] EnOrigin { get; set; }
+
+            [JsonProperty("zh_origin")]
+            public GameInfo[] ZhOrigin { get; set; }
+
+            [JsonProperty("zh_revision")]
+            public GameInfo[] ZhRevision { get; set; }
+        }
+
+        public partial class GameInfo
+        {
+            [JsonProperty("name")]
+            public string Name { get; set; }
+
+            [JsonProperty("version_type")]
+            public string VersionType { get; set; }
+
+            [JsonProperty("version")]
+            public string Version { get; set; }
+
+            [JsonProperty("execute_name")]
+            public string ExecuteName { get; set; }
+
+            [JsonProperty("url")]
+            public string Url { get; set; }
+        }
+    }
+}
