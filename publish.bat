64c2df5adab8b1cab8e7ecc01f6bf05f0c5cb13b
HuaMouRen <2825338723@qq.com>
2025-11-22 21:50:22 +0800

    build: 将更新服务添加进构建文件夹内

--- publish.bat
+++ publish.bat
@@ -2,3 +2,4 @@
 
 dotnet publish "PvzLauncherRemake\PvzLauncherRemake.csproj" -c Release -o "publish\bin"
 dotnet publish "ExecuteShell\ExecuteShell.csproj" -c Release -o "publish"
+dotnet publish "UpdateService\UpdateService.csproj" -c Release -o "publish\bin"
