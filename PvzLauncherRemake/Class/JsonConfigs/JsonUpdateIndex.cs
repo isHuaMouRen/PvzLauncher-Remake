14dd9002115300d629a0088b7dca193d0399e18b
HuaMouRen <2825338723@qq.com>
2025-11-22 20:50:56 +0800

    feat(json-update-index): 添加Json配置

---
+++ PvzLauncherRemake/Class/JsonConfigs/JsonUpdateIndex.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PvzLauncherRemake.Class.JsonConfigs
+{
+    public class JsonUpdateIndex
+    {
+        public class Index
+        {
+            [JsonProperty("stable")]   
+            public ChannelInfo Stable { get; set; }
+
+            [JsonProperty("development")]
+            public ChannelInfo Development { get; set; }
+        }
+
+        public class ChannelInfo
+        {
+            [JsonProperty("latest_version")]
+            public string LatestVersion { get; set; }
+
+            [JsonProperty("url")]
+            public string Url { get; set; }
+        }
+    }
+}
