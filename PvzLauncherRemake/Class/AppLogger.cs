e9d55c856e2bc3346375d58ef85580f15abedcf7
HuaMouRen <2825338723@qq.com>
2025-11-27 11:26:00 +0800

    imp(app-logger): 不写日期与调用位置

--- PvzLauncherRemake/Class/AppLogger.cs
+++ PvzLauncherRemake/Class/AppLogger.cs
@@ -6,6 +6,11 @@ namespace PvzLauncherRemake.Class
     public static class AppLogger
     {
         //日志记录器
-        public static Logger logger = new Logger(Path.Combine(AppInfo.ExecuteDirectory, "Logs")) { ShowCallerInfo = false };
+        public static Logger logger = new Logger
+        {
+            LogDirectory = Path.Combine(AppInfo.ExecuteDirectory, "Logs"),
+            ShowCallerInfo = false,
+            ShowDate = false
+        };
     }
 }
