ac8708ff329b3a12051feee4a3a0b0db650dcf1a
HuaMouRen <2825338723@qq.com>
2025-11-27 20:31:34 +0800

    feat(dialog-manager): 添加日志保护

--- PvzLauncherRemake/Utils/DialogManager.cs
+++ PvzLauncherRemake/Utils/DialogManager.cs
@@ -1,4 +1,5 @@
 ﻿using ModernWpf.Controls;
+using static PvzLauncherRemake.Class.AppLogger;
 
 namespace PvzLauncherRemake.Utils
 {
@@ -8,14 +9,22 @@ namespace PvzLauncherRemake.Utils
 
         public static async Task ShowDialogAsync(ContentDialog dialog, Action? primaryCallback = null, Action? secondaryCallback = null, Action? closeCallback = null)
         {
-            
+            logger.Info($"[对话框管理器] 显示对话框: Title=\"{dialog.Title}\" Content=\"{dialog.Content}\" PrimaryButton=\"{dialog.PrimaryButtonText}\" SecondaryButton=\"{dialog.SecondaryButtonText}\" CloseButton=\"{dialog.CloseButtonText}\"");
+            logger.Info($"[对话框管理器] 等待对话框空闲");
             // 等待直到没有对话框显示
             await WaitForDialogToCloseAsync();
-
+            logger.Info($"[对话框管理器] 显示对话框: {dialog.Title}");
             isDialogShow = true;
 
             var result = await dialog.ShowAsync();
-            
+            logger.Info($"[对话框管理器] 对话框关闭, 用户选择: {(
+                result == ContentDialogResult.Primary ? "Primary" :
+                result == ContentDialogResult.Secondary ? "Secondary" : "Close"
+            )}: {(
+                result == ContentDialogResult.Primary ? dialog.PrimaryButtonText :
+                result == ContentDialogResult.Secondary ? dialog.SecondaryButtonText : dialog.CloseButtonText
+            )}");
+
             isDialogShow = false;
             HandleDialogResult(result, primaryCallback, secondaryCallback, closeCallback);
         }
@@ -24,7 +33,6 @@ namespace PvzLauncherRemake.Utils
         {
             while (isDialogShow)
             {
-                
                 await Task.Delay(100);
             }
         }
