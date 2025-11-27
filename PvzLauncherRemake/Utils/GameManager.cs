a2100161f2687100f4a7bde70671d33f96c33b9a
HuaMouRen <2825338723@qq.com>
2025-11-27 20:46:13 +0800

    feat(game-manager): 添加日志保护

--- PvzLauncherRemake/Utils/GameManager.cs
+++ PvzLauncherRemake/Utils/GameManager.cs
@@ -2,6 +2,7 @@
 using PvzLauncherRemake.Class;
 using PvzLauncherRemake.Class.JsonConfigs;
 using System.IO;
+using static PvzLauncherRemake.Class.AppLogger;
 
 namespace PvzLauncherRemake.Utils
 {
@@ -14,7 +15,7 @@ namespace PvzLauncherRemake.Utils
         {
             await Task.Run(() =>
             {
-                
+                logger.Info($"[游戏管理器] 开始加载游戏列表");
                 //清理
                 AppInfo.GameList.Clear();
 
@@ -26,6 +27,7 @@ namespace PvzLauncherRemake.Utils
                     string configPath = Path.Combine(game, ".pvzl.json");
                     if (File.Exists(configPath))
                     {
+                        logger.Info($"[游戏管理器] 发现有效配置文件: {configPath}");
                         //创建独立存档
                         if (AppInfo.Config.SaveConfig.EnableSaveIsolation && !Directory.Exists(Path.Combine(game, ".save")))
                         {
@@ -50,7 +52,8 @@ namespace PvzLauncherRemake.Utils
         {
             await Task.Run(() =>
             {
-                
+                logger.Info($"[游戏管理器] 开始加载修改器列表");
+
                 //清理
                 AppInfo.TrainerList.Clear();
 
@@ -62,6 +65,7 @@ namespace PvzLauncherRemake.Utils
                     string configPath = Path.Combine(trainer, ".pvzl.json");
                     if (File.Exists(configPath))
                     {
+                        logger.Info($"[游戏管理器] 发现有效配置文件: {configPath}");
                         JsonTrainerInfo.Index configContent = Json.ReadJson<JsonTrainerInfo.Index>(configPath);
                         
                         AppInfo.TrainerList.Add(configContent);
