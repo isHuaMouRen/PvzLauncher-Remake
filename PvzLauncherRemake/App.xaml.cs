138533a4186502f98f2f03b60d8ee0ef3c354356
HuaMouRen <2825338723@qq.com>
2025-11-28 09:52:03 +0800

    feat(app): 退出时会写日志

--- PvzLauncherRemake/App.xaml.cs
+++ PvzLauncherRemake/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Configuration;
+﻿using PvzLauncherRemake.Class;
+using System.Configuration;
 using System.Data;
 using System.Windows;
 
@@ -9,6 +10,15 @@ namespace PvzLauncherRemake
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnExit(ExitEventArgs e)
+        {
+            base.OnExit(e);
+
+            if (e.ApplicationExitCode == 0)
+                AppLogger.logger.Info($"[应用程序] 应用程序正常退出(ExitCode: {e.ApplicationExitCode})");
+            else
+                AppLogger.logger.Warn($"[应用程序] 应用程序非正常退出(ExitCode: {e.ApplicationExitCode})");
+        }
     }
 
 }
