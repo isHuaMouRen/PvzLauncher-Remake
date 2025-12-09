c30e95054065aa4c59af3d58cac7dad0190336f8
HuaMouRen <2825338723@qq.com>
2025-12-09 18:56:12 +0800

    feat(app-downloader): 独立Downloader

---
+++ PvzLauncherRemake/Class/AppDownloader.cs
@@ -0,0 +1,12 @@
+﻿using HuaZi.Library.Downloader;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PvzLauncherRemake.Class
+{
+    public class AppDownloader
+    {
+        public static Downloader? downloader = null;
+    }
+}
