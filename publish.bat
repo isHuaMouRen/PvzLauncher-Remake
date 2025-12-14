f3c7658e9f2d31c6fa625dca79b29037680a26c3
HuaMouRen <2825338723@qq.com>
2025-12-14 11:27:24 +0800

    chore: 更新发布脚本

--- publish.bat
+++ publish.bat
@@ -1,5 +1,22 @@
 @echo off
+setlocal enabledelayedexpansion
 
 dotnet publish "PvzLauncherRemake\PvzLauncherRemake.csproj" -c Release -o "publish\bin"
 dotnet publish "ExecuteShell\ExecuteShell.csproj" -c Release -o "publish"
 dotnet publish "UpdateService\UpdateService.csproj" -c Release -o "publish\bin"
+
+
+set /p majorversion=MajorVersion: 
+set /p version=Version: 
+set /p codename=CodeName: 
+
+set "targetDir=Builds\%majorversion%\%codename%\%version%"
+
+mkdir "%targetDir%" 2>nul
+
+xcopy "publish\*.*" "%targetDir%\" /s /e /y /i
+
+del "Builds\%majorVersion%\%codename%\%version%\PvzLauncher.deps.json"
+del "Builds\%majorVersion%\%codename%\%version%\PvzLauncher.pdb"
+
+pause
