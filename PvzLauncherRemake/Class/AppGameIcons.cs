bce9b1310cd5020eb05caec64e15133d45282eb5
HuaMouRen <2825338723@qq.com>
2025-12-15 18:42:06 +0800

    feat(app-game-icons): 补全

--- PvzLauncherRemake/Class/AppGameIcons.cs
+++ PvzLauncherRemake/Class/AppGameIcons.cs
@@ -6,9 +6,21 @@ namespace PvzLauncherRemake.Class
 {
     public enum GameIcons
     {
-        Origin,
-        GOTY,
-        Beta,
-        Tat
+        Unknown,//unknown
+
+        Origin,//origin
+        GOTY,//goty
+        Steam,//steam
+        Test,//test
+        Beta,//beta
+        Ghtr,//ghtr
+        Dream,//dream
+        NineFive,//ninefive
+        Hybrid,//he
+        Fusion,//fe
+        Tat,//tat
+
+        PvzToolkit,//pvztoolkit
+        CheatEngine//ce
     }
 }
