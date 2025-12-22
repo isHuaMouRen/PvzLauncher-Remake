7b979c2df8852e469c5cd19053ac2e5ea6c5c3a7
HuaMouRen <2825338723@qq.com>
2025-12-22 18:57:50 +0800

    feat(about): 如果运行的是CI构建，那么会在版本号末尾添加 ` - CI`

--- PvzLauncherRemake/Pages/PageAbout.xaml.cs
+++ PvzLauncherRemake/Pages/PageAbout.xaml.cs
@@ -35,7 +35,7 @@ namespace PvzLauncherRemake.Pages
         {
             InitializeComponent();
 
-            textBlock_Version.Text = $"{AppInfo.Version}";
+            textBlock_Version.Text = $"{AppInfo.Version}{(AppInfo.IsCIBuild ? " - CI" : null)}";
         }
 
         public void GoToUrl(object sender, RoutedEventArgs e)
