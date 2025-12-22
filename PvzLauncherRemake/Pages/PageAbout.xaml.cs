73585381cfc4e0d4ea825a689f64b8efe5633086
HuaMouRen <2825338723@qq.com>
2025-12-22 19:13:14 +0800

    feat(about): 在 Debug 配置文件下在版本号后方加上 - Debug

--- PvzLauncherRemake/Pages/PageAbout.xaml.cs
+++ PvzLauncherRemake/Pages/PageAbout.xaml.cs
@@ -35,7 +35,7 @@ namespace PvzLauncherRemake.Pages
         {
             InitializeComponent();
 
-            textBlock_Version.Text = $"{AppInfo.Version}{(AppInfo.Arguments.isCIBuild ? " - CI" : null)}";
+            textBlock_Version.Text = $"{AppInfo.Version}{(AppInfo.Arguments.isCIBuild ? " - CI" : AppInfo.Arguments.isDebugBuild ? " - Debug" : null)}";
         }
 
         public void GoToUrl(object sender, RoutedEventArgs e)
