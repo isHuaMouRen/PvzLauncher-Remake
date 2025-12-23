bbe6c27b411ca683bf31f526c4b2381dcedbc585
HuaMouRen <2825338723@qq.com>
2025-12-23 19:52:36 +0800

    feat(i18n): 成**关于**页面部分本地化 & 更改文件夹结构

--- PvzLauncherRemake/I18N/WindowMain.Designer.cs
+++ PvzLauncherRemake/I18N/WindowMain.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace PvzLauncherRemake.I18N.WindowMain {
+namespace PvzLauncherRemake.I18N {
     using System;
     
     
@@ -22,14 +22,14 @@ namespace PvzLauncherRemake.I18N.WindowMain {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "18.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    public class Language {
+    public class WindowMain {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
         private static global::System.Globalization.CultureInfo resourceCulture;
         
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal Language() {
+        internal WindowMain() {
         }
         
         /// <summary>
@@ -39,7 +39,7 @@ namespace PvzLauncherRemake.I18N.WindowMain {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("PvzLauncherRemake.I18N.WindowMain.Language", typeof(Language).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("PvzLauncherRemake.I18N.WindowMain", typeof(WindowMain).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
