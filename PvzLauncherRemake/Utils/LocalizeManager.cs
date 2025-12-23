8b1514551d818037361957b352a1302b6726abc0
HuaMouRen <2825338723@qq.com>
2025-12-23 19:24:27 +0800

    feat(localize-manager): 添加语言管理器工具类

---
+++ PvzLauncherRemake/Utils/LocalizeManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using WPFLocalizeExtension.Engine;
+
+namespace PvzLauncherRemake.Utils
+{
+    public class LocalizeManager
+    {
+        public static void SwitchLanguage(string languageCode = "zh-CN")
+        {
+            var newCulture = new CultureInfo(languageCode);
+            Thread.CurrentThread.CurrentCulture = newCulture;
+            Thread.CurrentThread.CurrentUICulture = newCulture;
+
+            LocalizeDictionary.Instance.Culture = newCulture;
+        }
+    }
+}
