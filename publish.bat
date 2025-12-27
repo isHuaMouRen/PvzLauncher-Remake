64e5b9bea88782c51f6efabd9bbf919070cd15d4
HuaMouRen <2825338723@qq.com>
2025-12-27 13:49:04 +0800

    chore: 更新发布脚本

--- publish.bat
+++ publish.bat
@@ -1,31 +1,17 @@
 @echo off
 setlocal enabledelayedexpansion
 
+rd publish /s
+
 dotnet publish "PvzLauncherRemake\PvzLauncherRemake.csproj" -c Release -o "publish\bin"
 dotnet publish "ExecuteShell\ExecuteShell.csproj" -c Release -o "publish"
 dotnet publish "UpdateService\UpdateService.csproj" -c Release -o "publish\bin"
 
-
-set "DEFAULT_MAJORVERSION=1.0.0"
-set "DEFAULT_CODENAME=Release Candidate"
-
-set /p version=Version: 
-set /p codename=CodeName (%DEFAULT_CODENAME%): 
-set /p majorversion=MajorVersion (%DEFAULT_MAJORVERSION%): 
-if "%majorversion%"=="" set "majorversion=%DEFAULT_MAJORVERSION%"
-if "%codename%"=="" set "codename=%DEFAULT_CODENAME%"
-
-set "targetDir=Builds\%majorversion%\%codename%\%version%"
-
-mkdir "%targetDir%" 2>nul
-
-xcopy "publish\*.*" "%targetDir%\" /s /e /y /i
-
-del "%targetDir%\PvzLauncher.deps.json"
-del "%targetDir%\PvzLauncher.pdb"
-del "%targetDir%\bin\PvzLauncherRemake.deps.json"
-del "%targetDir%\bin\PvzLauncherRemake.pdb"
-del "%targetDir%\bin\UpdateService.deps.json"
-del "%targetDir%\bin\UpdateService.pdb"
+del "publish\PvzLauncher.deps.json"
+del "publish\PvzLauncher.pdb"
+del "publish\bin\PvzLauncherRemake.deps.json"
+del "publish\bin\PvzLauncherRemake.pdb"
+del "publish\bin\UpdateService.deps.json"
+del "publish\bin\UpdateService.pdb"
 
 pause
